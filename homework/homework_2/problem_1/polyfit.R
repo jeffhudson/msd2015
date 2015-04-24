a61c24a32932b3a33a0c750e02bcca17aca5963d
jeffhudson <jdh2182@columbia.edu>
2015-04-24 09:10:04 -0400

    adding hw2 solutions
    Former-commit-id: a91e7fac8f0e77f0c4df722bf5461801fd33f5b4

--- homework/homework_2/problem_1/polyfit.R
+++ homework/homework_2/problem_1/polyfit.R
@@ -1,14 +1,77 @@
+rm(list=ls())
 library(ggplot2)
+library(dplyr)
+theme_set(theme_bw())
 
+setwd("C:/Users/Jeff.Bernard/Dropbox/QMSS/gitpages/msd2015/homework/homework_2/problem_1/")
 # read data
 data <- read.table('polyfit.tsv', header=T)
 
 # split into train / test data
+setseed(101)
+ttsplit <- 0.5 # given in problem description
+test <- sample.int(nrow(data),nrow(data)*ttsplit)
+testset <- data[test,]
+trainset <- data[-test,]
 
 # fit a model and compute train / test error for each degree
+fit_poly <- function(trainset,testset,num_poly=50){
+  fit <- list()
+  RMSE <- matrix(0,nrow=num_poly,ncol=3)
+  for(i in 1:num_poly){
+    trainpoly <- as.data.frame(cbind(y=trainset$y,poly(trainset$x,i,raw=T)))
+    testpoly <- as.data.frame(cbind(y=testset$y,poly(testset$x,i,raw=T)))
+    fit[[i]] <- lm(y ~ ., data=trainpoly)
+    trnRMSE <- sqrt(mean(fit[[i]]$residuals^2))
+    tstRMSE <- sqrt(mean((testpoly$y - (predict(fit[[i]],newdata=testpoly)))^2))
+    RMSE[i,] <- cbind(i,trnRMSE,tstRMSE)
+  }
+  return(list(fit,RMSE))
+}
+
+results <- fit_poly(trainset,testset)
+RMSE <- as.data.frame(results[[2]])
+which.min(RMSE$V3)
+min(RMSE$V3)
+bestmodel <- results[[1]][[bestpoly]]
+
 
 # select best model
 
+## run 50 times to ensure best polynomial fit is robust to test split idiosyncrasy
+find_best_poly <- function(n,data){
+  bestpoly <- rep(0,n)
+  for(i in 1:n){
+    test <- sample.int(nrow(data),nrow(data)*ttsplit)
+    testset <- data[test,]
+    trainset <- data[-test,]
+    results <- fit_poly(trainset,testset)
+    RMSE <- as.data.frame(results[[2]])
+    bestpoly[i] <- which.min(RMSE$V3)
+  }
+  return(bestpoly)
+}
+
+CV <- find_best_poly(50,data)
+bestpoly <- table(CV) %>% which.max %>% names %>% as.numeric
+
+ggplot(RMSE, aes(x=V1)) + 
+  geom_line(aes(y=V2), color="darkred") + 
+  geom_line(aes(y=V3), color="darkblue") +
+  annotate("text",label="Training Error", x=40, y=0.014, color="darkred") + 
+  annotate("text",label="Testing Error", x=40, y=0.033, color="darkblue") +
+  labs(title="Error as function of polynomial degree",y="RMSE",x="Polynomial Degree")
+
 # plot fit for best model
+model <- function(x,coefs) {
+  y <- coefs[1]
+  for(i in 1:bestpoly){
+    y <- y + (x^(i))*coefs[i+1]
+  }
+  return(y)
+}
+ggplot(data) + geom_point(aes(x,y)) + 
+  stat_function(fun=model,args=list(coefs=bestmodel$coef),color="red",size=1)
 
 # report coefficients for best model
+coefs
--- homework/homework_2/problem_2/classify_nyt_articles.R
+++ homework/homework_2/problem_2/classify_nyt_articles.R
@@ -3,13 +3,22 @@ library(Matrix)
 library(glmnet)
 library(ROCR)
 library(ggplot2)
+library(dplyr)
 
 # read business and world articles into one data frame
+rm(list=ls())
+setwd("C:/Users/Jeff.Bernard/Dropbox/QMSS/gitpages/msd2015/homework/homework_2/problem_2/")
+bizns <- read.table("business.tsv", fileEncoding="UTF-8")
+world <- read.table("world.tsv", fileEncoding="UTF-8")
 
 # create a Corpus from the article snippets
+VS <- VectorSource(c(as.character(bizns$snippet),as.character(world$snippet)))
+
+# remove punctuation and numbers
+Corp <- VCorpus(VS) %>% tm_map(removeNumbers) %>% tm_map(removePunctuation)
 
 # create a DocumentTermMatrix from the snippet Corpus
-# remove punctuation and numbers
+dtm <- DocumentTermMatrix(Corp)
 
 # convert the DocumentTermMatrix to a sparseMatrix, required by cv.glmnet
 # helper function
@@ -17,13 +26,34 @@ dtm_to_sparse <- function(dtm) {
  sparseMatrix(i=dtm$i, j=dtm$j, x=dtm$v, dims=c(dtm$nrow, dtm$ncol), dimnames=dtm$dimnames)
 }
 
+sp <- dtm_to_sparse(dtm)
+
 # create a train / test split
 
+set.seed(101)
+ttsplit <- 0.2
+test <- sample.int(nrow(sp),nrow(sp)*ttsplit)
+testlabels <- test < 1001
+trainlabels <- (1:nrow(sp))[-test] < 1001
+testset <- sp[test,]
+trainset <- sp[-test,]
+
 # cross-validate logistic regression with cv.glmnet, measuring auc
+crossval <- cv.glmnet(trainset, trainlabels, family="binomial", type.measure="auc", nfolds=10)
 
 # evaluate performance for the best-fit model
+plot(crossval)
 
 # plot ROC curve and output accuracy and AUC
+preds <- predict(crossval,newx=testset)
+pred.obj <- prediction(preds,testlabels)
+ROC <- performance(pred.obj,measure="tpr",x.measure="fpr")
+plot(ROC)
+acc <- performance(pred.obj,measure="acc")
+plot(acc)
+max(acc@y.values[[1]])
+AUC <- performance(pred.obj,measure="auc")@y.values[[1]]
+
 
 # extract coefficients for words with non-zero weight
 # helper function
@@ -36,6 +66,11 @@ get_informative_words <- function(crossval) {
   subset(coefs, weight != 0)
 }
 
+infowds <- get_informative_words(crossval)
+sorted <- arrange(infowds,weight)
+
 # show weights on words with top 10 weights for business
+arrange(tail(sorted,10),desc(weight))
 
 # show weights on words with top 10 weights for world
+head(sorted,10)
